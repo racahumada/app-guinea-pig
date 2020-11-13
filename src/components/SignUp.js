3db8b81bbcfc2103812e33d4f4626b7ae8300af3
Ricardo Andres Castro Ahumada <ricardocastro.ti@gmail.com>
2020-11-13 17:39:13 -0300

    Arquivo signUp.js modificando uso de css

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,37 +1,53 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
-import hostServices from '../services/hostServices.js'
-import css from './signup.module.css'
+import hostServices from '../services/hostServices.js';
+import './signup.css';
 
 const SignUp = () => {
-  const {register, handleSubmit, errors} = useForm();
+  const { register, handleSubmit, errors } = useForm();
 
   const onSubmit = async (data) => {
     console.log(data);
-    const dataTest = await hostServices.postSignUp(data)
+    const dataTest = await hostServices.postSignUp(data);
     console.log(dataTest);
   };
+
   return (
-    <div className={css.boxSignup}>
-      <div className={css.logo}>
-        <img src="./img/guinea-pig-head.png" alt="Logo Guinea App"/>
+    <div className="boxSignup">
+      <div className="logo">
+        <img src="./img/guinea-pig-head.png" alt="Logo Guinea App" />
       </div>
       <h3>CADASTRO</h3>
-      <form onSubmit={handleSubmit(onSubmit)} className={css.boxForm}>
+      <form onSubmit={handleSubmit(onSubmit)} className="boxForm">
         <label>Nome:</label>
-        <input type="text" name="name" id="name" placeholder="Seu Nome" ref={register({required: true})} />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Seu Nome"
+          ref={register({ required: true })}
+        />
         {errors.name && <span>Campo de nome é Obrigatório</span>}
         <label>Email:</label>
-        <input type="text" name="email" placeholder="example@guinea.com" ref={register({required: true})} />
+        <input
+          type="text"
+          name="email"
+          placeholder="example@guinea.com"
+          ref={register({ required: true })}
+        />
         {errors.email && <span>Campo de email é obrigatório</span>}
         <label>Senha:</label>
-        <input type="password" name="password" placeholder="**********" ref={register({required: true})} />
+        <input
+          type="password"
+          name="password"
+          placeholder="**********"
+          ref={register({ required: true })}
+        />
         {errors.password && <span>Campo de senha obrigatório</span>}
         <button>Cadastrar</button>
       </form>
     </div>
   );
-
 };
 
 export default SignUp;
